429e3529d06edec32f36fb7b4c587ac22bac8797
Midhun-Nk <midhunnk2021@gmail.com>
2026-06-27 23:24:54 +0530

    Updated Community

--- ReLearn/count_of_num.py
+++ ReLearn/count_of_num.py
@@ -1,39 +1,13 @@
-numberlist1 = [1,1,2,3,4,6,3,4,3]
-numberlist2 = [1,1,2,3,4,53,3,4,3]
 
-feq3 = {}
+def second(num):
+    for i in range(0,len(num)):
+        next_no = 0
+        j=i
+        while (num[i] <= num[i+1]):
+            next_no = j
+            j+=1
+        if next_no != 0:
+            num[i],num[next_no] = num[next_no],num[i] 
+    print(num)
 
-
-feq = {}
-feq2 = {}
-
-    
-for i in numberlist2:
-    count = 0
-    for j in numberlist1:
-        # print(i,j)
-        if i == j:
-            count +=1
-    feq[i] = count
-
-hashed_list = [0]*11
-
-for i in numberlist1:
-    hashed_list[i] +=1
-
-for j in numberlist2:
-    if j < 0 or j >10:
-        feq2[j] = 0
-    else:
-        feq2[j] = hashed_list[j]
-
-
-print(feq)
-print(feq2)
-print(feq3)
-
-
-
-
-
-
+second([1,1,1,2,3,4,4,4,5,6,7 ])
